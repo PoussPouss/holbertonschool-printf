fbe90374687e9bc0691b8176e4725ad66b258cb0
PoussPouss <guillaume_font@orange.fr>
2025-03-26 15:59:17 +0100

    Ajout d'une vérification pour éviter un retour incorrect dans la fonction _printf lors d'un format de chaîne incomplète

--- _printf.c
+++ _printf.c
@@ -18,12 +18,12 @@ int _printf(const char *format, ...)
 	if (!format)
 		return (-1);
 	va_start(args, format);
-
 	while (format[i] != '\0')
 	{
 		if (format[i] == '%')
 		{
-
+			if (format[i + 1] == '\0')
+			return (-1);
 			if (format[i + 1] == '%')
 			{
 				_putchar('%');
